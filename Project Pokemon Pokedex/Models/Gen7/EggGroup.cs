98e36796d270ac1b363405fb35833fbd1fc25ffa
evandixon <evanldixon@live.com>
2018-03-23 13:59:24 -0500

    Add code to gather Egg Group data

---
+++ Project Pokemon Pokedex/Models/Gen7/EggGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectPokemon.Pokedex.Models.Gen7
+{
+    public class EggGroup
+    {
+        public string Name { get; set; }
+        public List<Pokemon> SingleEggGroupPokemon { get; set; }
+        public List<Pokemon> MultiEggGroupPokemon { get; set; }
+    }
+}
