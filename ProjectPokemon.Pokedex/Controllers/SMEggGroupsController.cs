655d9137d44fe1530b9bb0673ef27fa13b777a22
Dixon, Evan <evanldixon@gmail.com>
2019-05-24 16:48:44 -0500

    Port Gen 7 Egg Groups

---
+++ ProjectPokemon.Pokedex/Controllers/SMEggGroupsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjectPokemon.Pokedex.Models.Games;
+using ProjectPokemon.Pokedex.Models.Games.Gen7;
+
+namespace ProjectPokemon.Pokedex.Controllers
+{
+    [Route("sm/egggroups")]
+    public class SMEggGroupsController : Controller
+    {
+        public SMEggGroupsController(DataCollection data)
+        {
+            _data = data?.SMData ?? throw new ArgumentNullException();
+        }
+
+        private Gen7DataCollection _data;
+
+        // GET: sm/egggroups
+        public ActionResult Index()
+        {
+            return View(_data.GetEggGroups());
+        }
+
+        // GET: sm/egggroups/{id}
+        [Route("{id}")]
+        public ActionResult Details(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return BadRequest();
+            }
+
+            var eggGroup = _data.GetEggGroups().FirstOrDefault(eg => StringComparer.OrdinalIgnoreCase.Compare(eg.Name, id) == 0);
+            if (eggGroup == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.ControllerPrefix = "SM";           
+
+            return View(eggGroup);
+        }
+    }
+}
---
+++ ProjectPokemon.Pokedex/Controllers/USUMEggGroupsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectPokemon.Pokedex.Models.Games;
+using ProjectPokemon.Pokedex.Models.Games.Gen7;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectPokemon.Pokedex.Controllers
+{
+    [Route("usum/egggroups")]
+    public class USUMEggGroupsController : Controller
+    {
+        public USUMEggGroupsController(DataCollection data)
+        {
+            _data = data?.SMData ?? throw new ArgumentNullException();
+        }
+
+        private readonly Gen7DataCollection _data;
+
+        // GET: usum/egggroups
+        public ActionResult Index()
+        {
+            return View(_data.GetEggGroups());
+        }
+
+        // GET: usum/egggroups/{id}
+        [Route("{id}")]
+        public ActionResult Details(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return BadRequest();
+            }
+
+            var eggGroup = _data.GetEggGroups().FirstOrDefault(eg => StringComparer.OrdinalIgnoreCase.Compare(eg.Name, id) == 0);
+            if (eggGroup == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.ControllerPrefix = "USUM";
+
+            return View(eggGroup);
+        }
+    }
+}
